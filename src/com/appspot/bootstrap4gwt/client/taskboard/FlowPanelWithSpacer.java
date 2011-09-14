c7a12c5e8b48c6d055b999ffbb8d1eb49778abab
yusuke.kokubo <yusuke.kokubo@gmail.com>
2011-09-14 14:15:44 +0900

    code format

--- src/com/appspot/bootstrap4gwt/client/taskboard/FlowPanelWithSpacer.java
+++ src/com/appspot/bootstrap4gwt/client/taskboard/FlowPanelWithSpacer.java
@@ -6,23 +6,23 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class FlowPanelWithSpacer extends FlowPanel {
 
-  public FlowPanelWithSpacer() {
-    HTML spacerLabel = new HTML("<hr />");
-    spacerLabel.setHeight("50px");
-    super.add(spacerLabel);
-  }
-
-  @Override
-  public void add(Widget w) {
-    super.insert(w, getWidgetCount() - 1);
-  }
-
-  @Override
-  public void insert(Widget w, int beforeIndex) {
-    if (beforeIndex == getWidgetCount()) {
-      beforeIndex--;
+    public FlowPanelWithSpacer() {
+        HTML spacerLabel = new HTML("<hr />");
+        spacerLabel.setHeight("50px");
+        super.add(spacerLabel);
     }
-    super.insert(w, beforeIndex);
-  }
+
+    @Override
+    public void add(Widget w) {
+        super.insert(w, getWidgetCount() - 1);
+    }
+
+    @Override
+    public void insert(Widget w, int beforeIndex) {
+        if (beforeIndex == getWidgetCount()) {
+            beforeIndex--;
+        }
+        super.insert(w, beforeIndex);
+    }
 
 }
