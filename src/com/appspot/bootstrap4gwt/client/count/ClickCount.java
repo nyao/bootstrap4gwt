e3d56cb5a155d68068e27faa4e869f14cc8f1e47
y.kokubo <y.kokubo@otr-jp.com>
2012-03-08 10:56:40 +0900

    implement with new widget

--- src/com/appspot/bootstrap4gwt/client/count/ClickCount.java
+++ src/com/appspot/bootstrap4gwt/client/count/ClickCount.java
@@ -1,21 +1,21 @@
 package com.appspot.bootstrap4gwt.client.count;
 
 
+import com.appspot.bootstrap4gwt.client.ui.ButtonPrimary;
+import com.appspot.bootstrap4gwt.client.ui.UneditableInput;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.user.client.ui.Label;
 
 public class ClickCount extends Composite {
 	
 	private static HelloUiBinder uiBinder = GWT.create(HelloUiBinder.class);
-	@UiField Button button;
-	@UiField Label label;
+	@UiField ButtonPrimary button;
+	@UiField UneditableInput label;
 
 	interface HelloUiBinder extends UiBinder<Widget, ClickCount> {
 	}
