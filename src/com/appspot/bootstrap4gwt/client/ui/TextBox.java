7b8fa7f4a6fcf9c82a81b5c429702be7c69b5cb0
y.kokubo <y.kokubo@otr-jp.com>
2012-03-09 11:07:58 +0900

    remove DisabledInput

--- src/com/appspot/bootstrap4gwt/client/ui/TextBox.java
+++ src/com/appspot/bootstrap4gwt/client/ui/TextBox.java
@@ -1,49 +1,40 @@
 package com.appspot.bootstrap4gwt.client.ui;
 
-import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.InputElement;
 
 public class TextBox extends com.google.gwt.user.client.ui.TextBox {
 
     public TextBox() {
-        this(Document.get().createTextInputElement(), "input-xlarge");
+        super();
+        setStyleName("");
     }
 
     protected TextBox(Element element) {
-      super(element);
-      assert InputElement.as(element).getType().equalsIgnoreCase("text");
+        super(element);
     }
 
-    TextBox(Element element, String styleName) {
-      super(element);
-      if (styleName != null) {
-        setStyleName(styleName);
-      }
-    }
-    
     public void setId(String id) {
         getElement().setAttribute("id", id);
     }
-    
+
     public void setPlaceholder(String s) {
         getElement().setAttribute("placeholder", s);
     }
-    
+
     public void setDisabled(String s) {
         getElement().setAttribute("disabled", s);
     }
-    
+
     public void setWarning(String s) {
         Element elm = this.getElement().getParentElement().getParentElement();
         elm.addClassName("warning");
     }
-    
+
     public void setError(String s) {
         Element elm = getElement().getParentElement().getParentElement();
         elm.addClassName("error");
     }
-    
+
     public void setSuccess(String s) {
         Element elm = getElement().getParentElement().getParentElement();
         elm.addClassName("success");
