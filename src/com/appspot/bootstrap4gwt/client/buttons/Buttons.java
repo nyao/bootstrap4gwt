c90e1f4d3677ca70b0d648b969b6d43b5b30f78a
y.kokubo <y.kokubo@otr-jp.com>
2012-03-09 13:50:04 +0900

    add click sample for Buttons

--- src/com/appspot/bootstrap4gwt/client/buttons/Buttons.java
+++ src/com/appspot/bootstrap4gwt/client/buttons/Buttons.java
@@ -1,7 +1,12 @@
 package com.appspot.bootstrap4gwt.client.buttons;
 
+import com.appspot.bootstrap4gwt.client.ui.Button;
+import com.appspot.bootstrap4gwt.client.ui.ButtonType;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -15,5 +20,48 @@ public class Buttons extends Composite {
     public Buttons() {
         initWidget(uiBinder.createAndBindUi(this));
     }
+    
+    @UiField Button sample;
 
+    @UiHandler("defaultButton")
+    void onDefaultClick(ClickEvent event) {
+        sample.setType(ButtonType.Default);
+        sample.setText(sample.getType().name());
+    }
+
+    @UiHandler("primary")
+    void onPrimaryClick(ClickEvent event) {
+        sample.setType(ButtonType.Primary);
+        sample.setText(sample.getType().name());
+    }
+
+    @UiHandler("info")
+    void onInfoClick(ClickEvent event) {
+        sample.setType(ButtonType.Info);
+        sample.setText(sample.getType().name());
+    }
+
+    @UiHandler("success")
+    void onSuccessClick(ClickEvent event) {
+        sample.setType(ButtonType.Success);
+        sample.setText(sample.getType().name());
+    }
+
+    @UiHandler("warning")
+    void onWarningClick(ClickEvent event) {
+        sample.setType(ButtonType.Warning);
+        sample.setText(sample.getType().name());
+    }
+
+    @UiHandler("danger")
+    void onDangerClick(ClickEvent event) {
+        sample.setType(ButtonType.Danger);
+        sample.setText(sample.getType().name());
+    }
+
+    @UiHandler("inverse")
+    void onInverseClick(ClickEvent event) {
+        sample.setType(ButtonType.Inverse);
+        sample.setText(sample.getType().name());
+    }
 }
