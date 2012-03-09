548614ea6226d51970d5fac0253db49c90b89882
y.kokubo <y.kokubo@otr-jp.com>
2012-03-09 16:38:23 +0900

    bring EntryPoints

---
+++ src/com/appspot/bootstrap4gwt/client/EntryPointForSample.java
@@ -0,0 +1,20 @@
+package com.appspot.bootstrap4gwt.client;
+
+import com.appspot.bootstrap4gwt.client.breadcrumbs.Breadcrumbs;
+import com.appspot.bootstrap4gwt.client.buttons.Buttons;
+import com.appspot.bootstrap4gwt.client.celltable.MyCellTable;
+import com.appspot.bootstrap4gwt.client.forms.Forms;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class EntryPointForSample implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        RootPanel.get("CellTable").add(new MyCellTable());
+        RootPanel.get("Buttons").add(new Buttons());
+        RootPanel.get("Forms").add(new Forms());
+        RootPanel.get("Breadcrumbs").add(new Breadcrumbs());
+    }
+
+}
