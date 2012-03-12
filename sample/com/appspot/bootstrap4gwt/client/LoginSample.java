aa018fb28c61d7e87b741c7a78a477febaaca2d3
y.kokubo <y.kokubo@otr-jp.com>
2012-03-12 14:38:17 +0900

    remove login rpc

--- sample/com/appspot/bootstrap4gwt/client/LoginSample.java
+++ sample/com/appspot/bootstrap4gwt/client/LoginSample.java
@@ -1,8 +1,5 @@
 package com.appspot.bootstrap4gwt.client;
 
-import com.appspot.bootstrap4gwt.client.service.LoginService;
-import com.appspot.bootstrap4gwt.client.service.LoginServiceAsync;
-import com.appspot.bootstrap4gwt.shared.model.Login;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.Window;
@@ -13,33 +10,20 @@ import com.google.gwt.user.client.ui.RootPanel;
 
 public class LoginSample implements EntryPoint {
 
-    LoginServiceAsync service = GWT.create(LoginService.class);
-
     public void onModuleLoad() {
-        service.login(Window.Location.getHref(), new AsyncCallback<Login>() {
-            @Override
-            public void onSuccess(Login login) {
-                if (login.isUserLoggedIn) {
-                    RootPanel.get("UserName").add(new InlineLabel(login.login));
-                    RootPanel.get("link2loginout").add(new Anchor("signout", login.logoutUrl));
-                    RootPanel.get("UserNameOnBoard").add(new InlineLabel(login.login));
-                    Anchor w = new Anchor("signout", login.logoutUrl);
+                    RootPanel.get("UserName").add(new InlineLabel("hoge@foo.com"));
+                    RootPanel.get("link2loginout").add(new Anchor("signout", "#"));
+                    RootPanel.get("UserNameOnBoard").add(new InlineLabel("hoge@foo.com"));
+                    Anchor w = new Anchor("signout", "#");
                     w.addStyleName("btn btn-success");
                     RootPanel.get("link2loginoutOnBoard").add(w);
-                } else {
-                    RootPanel.get("UserName").add(new InlineLabel("guest(please signin)"));
-                    RootPanel.get("link2loginout").add(new Anchor("signin", login.loginUrl));
-                    RootPanel.get("UserNameOnBoard").add(new InlineLabel("guest(please signin)"));
-                    Anchor w = new Anchor("signin", login.loginUrl);
-                    w.addStyleName("btn btn-primary");
-                    RootPanel.get("link2loginoutOnBoard").add(w);
-                }
-            }
+                    
+//                    RootPanel.get("UserName").add(new InlineLabel("guest(please signin)"));
+//                    RootPanel.get("link2loginout").add(new Anchor("signin", login.loginUrl));
+//                    RootPanel.get("UserNameOnBoard").add(new InlineLabel("guest(please signin)"));
+//                    Anchor w = new Anchor("signin", login.loginUrl);
+//                    w.addStyleName("btn btn-primary");
+//                    RootPanel.get("link2loginoutOnBoard").add(w);
 
-            @Override
-            public void onFailure(Throwable caught) {
-                Window.alert(caught.getMessage());
-            }
-        });
     }
 }
